727db1fa7f851400a6b8d68f89e2e688b468ea3d
debasish <dmdebasish07@gmail.com>
2024-11-20 11:41:41 +0530

    Architect Asian Develoement

--- src/pwc/java/assiignments/Assignment_1.java
+++ src/pwc/java/assiignments/Assignment_1.java
@@ -2,11 +2,21 @@ package pwc.java.assiignments;
 
 public class Assignment_1 {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Welcome to Git Repository Practice");
-		
+
 		System.out.println("Let's push to a new branch");
+		
+		Assignment_1 ob = new Assignment_1();
+		ob.NativeAppAndroid();
+		ob.NativeAppIos();
 	}
 
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+
+	public void NativeAppIos() {
+		System.out.println("NativeAppIos");
+	}
 }
