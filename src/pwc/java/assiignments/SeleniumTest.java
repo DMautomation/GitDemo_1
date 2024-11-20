6a6e56a8843587be64acfec3809009477587e934
debasish <dmdebasish07@gmail.com>
2024-11-20 12:01:09 +0530

    Architect American Development

---
+++ src/pwc/java/assiignments/SeleniumTest.java
@@ -0,0 +1,23 @@
+package pwc.java.assiignments;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		SeleniumTest obj = new SeleniumTest();
+		obj.browerAutomation();
+		obj.actionEle();
+		obj.elementUi();
+	}
+
+	public void browerAutomation() {
+		System.out.println("browerAutomation");
+	}
+
+	public void elementUi() {
+		System.out.println("elementUi");
+	}
+
+	public void actionEle() {
+		System.out.println("actionEle");
+	}
+}
